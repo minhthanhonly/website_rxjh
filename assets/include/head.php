759cae552210561080950dfc74c65260d7acb6ad
Caily.Thanh <thanhonly@caily.com.vn>
2023-08-01 14:04:02 +0700

    remove old temp

---
+++ assets/include/head.php
@@ -0,0 +1,40 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>.:: Hiệp Khách Giang Hồ ::.</title>
+<link rel="shortcut icon" href="http://yulgang.mgame.com/favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="google-site-verification" content="pREndLVVRadvwpFjs6eOgxzKXLUFwBzxlimyHVU-8m8" />
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127045668-1"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-127045668-1');
+function killCopy(e) {
+    return false
+}
+function reEnable() {
+    return true
+}
+document.onselectstart = new Function(“return false”)
+
+if (window.sidebar) {
+    document.onmousedown = killCopy
+    document.onclick = reEnable
+</script>
+
+<meta name="keywords"
+content="hiepkhach.net.vn,hk-dv.net,hkkteam,hiepkhach,hiep khach,hiepkhachgiangho,hiep khach giang ho,hiep khach dv,hiep khach kteam,hk k team,hk kteam,hkgh,yulgang online,yulgang">
+<!-- START: Facebook Meta -->
+<meta property="fb:app_id" content="">
+<meta property="og:title" content=".: Hiệp Khách Giang Hồ - Hồi Ức Thanh Xuân :.">
+<meta property="og:type" content="game">
+<meta property="og:url" content="http://hiepkhach.net.vn">
+<meta property="og:image" content="http://hiepkhach.net.vn//imgfb.png">
+<meta property="og:site_name" content="hiepkhachgiangho.vn">
+<meta name="description" content="Hiệp Khách Giang Hồ, Hiệp Khách Kteam Hiệp Khách v17, Hiệp Khách Thăng Thiên 5, hkkteam" />
+<meta property="og:description" content="Hiệp Khách Giang Hồ, Hiệp Khách Kteam, Hiệp Khách v20 Hiệp Khách Thăng Thiên 5, hkkteam" />
+<!-- END: Facebook Meta -->
+
+
