7761db6f9e146122babec6c6ab21811ba815e83b
Caily.Thanh <thanhonly@caily.com.vn>
2025-02-25 10:22:20 +0700

    Update footer.php

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -4,9 +4,9 @@
 			<div class="l-footer__nav">
 				<ul class="lst-link">
 					<li><a href="/?a=webshop"><img src="/assets/images/icn-shop.png" alt="">Cửa Hàng</a></li>
-					<li><a href="https://www.facebook.com/ThoLamGames" target="_blank"><img src="/assets/images/icn-support.png" alt="">Hỗ Trợ</a></li>
-					<li><a href="https://www.facebook.com/ThoLamGames" target="_blank"><img src="/assets/images/icn-fb2.png" alt="">Fanpage</a></li>
-					<li><a href="https://www.facebook.com/ThoLamGames" target="_blank"><img src="/assets/images/icn-group.png" alt="">FB Group</a></li>
+					<li><a href="https://www.facebook.com/minhthanhonly" target="_blank"><img src="/assets/images/icn-support.png" alt="">Hỗ Trợ</a></li>
+					<li><a href="https://www.facebook.com/minhthanhonly" target="_blank"><img src="/assets/images/icn-fb2.png" alt="">Fanpage</a></li>
+					<li><a href="https://www.facebook.com/minhthanhonly" target="_blank"><img src="/assets/images/icn-group.png" alt="">FB Group</a></li>
 					<li><a href="#" target="_blank"><img src="/assets/images/icn-zalo.png" alt=""></a></li>
 				</ul>
 				<!-- <ul class="lst-icon">
@@ -15,8 +15,8 @@
 				</ul> -->
 			</div>
 			<div class="l-footer__copyright">
-				<p class="right">&copy; Hiệp Khách VNC Games Entertainment. <?php echo date('Y');?></p>
-				<p class="right">hiepkhach.net.vn</p>
+				<p class="right">&copy; Hiệp Khách ONLY Games Entertainment. <?php echo date('Y');?></p>
+				<p class="right">hiepkhach.only</p>
 			</div>
 		</div>
 	</div>
